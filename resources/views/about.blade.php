2e06735fc70ffad44e50e2de328ed8f49dd3e04d
Giuseppe <laxeer0@gmail.com>
2021-01-25 11:14:48 +0100

    completed

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+  
+@extends("layout.layout")
+@section("title","Home")
+
+
+@section("main")
+
+<p>This is an about section</p>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+  
+@extends("layout.layout")
+@section("title","Home")
+
+
+@section("main")
+
+<p>This is a home section</p>
+
+@endsection
